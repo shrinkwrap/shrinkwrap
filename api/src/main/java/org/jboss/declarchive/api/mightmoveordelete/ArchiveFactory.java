5f58d16bbcdaaec877492f45b0a1ddcd558caffd
Andrew Lee Rubinger <alr@jboss.org>
2009-08-17 10:59:12 +0000

    [TMPARCH-6] Clear the way for Aslak and Container IFs

--- api/src/main/java/org/jboss/declarchive/api/mightmoveordelete/ArchiveFactory.java
+++ api/src/main/java/org/jboss/declarchive/api/mightmoveordelete/ArchiveFactory.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.declarchive.api.jar;
+package org.jboss.declarchive.api.mightmoveordelete;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
