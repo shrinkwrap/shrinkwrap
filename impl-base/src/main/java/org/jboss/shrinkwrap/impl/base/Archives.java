454b93e10c53242c226d4c58a7699aca26e344d9
Aslak Knutsen <aslak@redhat.com>
2009-11-10 15:16:40 +0000

    SHRINKWRAP-50 Unified factory for archive creation

---
+++ impl-base/src/main/java/org/jboss/shrinkwrap/impl/base/Archives.java
@@ -0,0 +1,51 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.shrinkwrap.impl.base;
+
+import org.jboss.shrinkwrap.api.Specializer;
+
+/**
+ * Generic unified factory for archive creation.
+ *
+ * @author <a href="mailto:aslak@conduct.no">Aslak Knutsen</a>
+ * @version $Revision: $
+ */
+public class Archives
+{
+   //-------------------------------------------------------------------------------------||
+   // Class Members ----------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   /**
+    * Create a Specialized archive base from where any other archive type can be made.
+    * 
+    * @param archiveName The name of the archive
+    * @return A {@link Specializer} archive base  
+    */
+   public static Specializer create(String archiveName) {
+      return new MemoryMapArchiveImpl(archiveName);
+   }
+   
+   //-------------------------------------------------------------------------------------||
+   // Constructor ------------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   /**
+    * No instantiation
+    */
+   private Archives() {}
+}
---
+++ impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/ArchivesTest.java
@@ -0,0 +1,53 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.shrinkwrap.impl.base;
+
+import junit.framework.Assert;
+
+import org.jboss.shrinkwrap.api.Specializer;
+import org.jboss.shrinkwrap.spi.MemoryMapArchive;
+import org.junit.Test;
+
+
+/**
+ * ArchivesTest
+ *
+ * @author <a href="mailto:aslak@conduct.no">Aslak Knutsen</a>
+ * @version $Revision: $
+ */
+public class ArchivesTest
+{
+
+   @Test
+   public void shouldBeAbleToCreateANewArchive() throws Exception {
+      String archiveName = "test.war";
+      
+      Specializer archive = Archives.create(archiveName);
+      
+      Assert.assertNotNull(
+            "A archive should have been created", archive);
+      
+      Assert.assertTrue(
+            "A MemoryMapArchive should have been created", 
+            archive instanceof MemoryMapArchive);
+      
+      Assert.assertEquals(
+            "Should have the same name as given imput", 
+            archiveName, 
+            ((MemoryMapArchive)archive).getName());
+   }
+}
