767e2cc40883d04a0a1243ae371bce439cc8803b
Andrew Lee Rubinger <alr@jboss.org>
2010-05-26 16:04:19 +0000

    [SHRINKWRAP-163] Make archive name optional in ShrinkWrap.create and ArchiveFactory.create.  Original patch by Ken Gullaksen with some adjustments by ALR: Added UnknownExtensionTypeException Made JavaDoc more explicit Removed impl-base test class to get the extension names; the test should define these on its own so we do not pass the test if the API accidentally changes extension mappings

---
+++ api/src/main/java/org/jboss/shrinkwrap/api/UnknownExtensionTypeException.java
@@ -0,0 +1,75 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.shrinkwrap.api;
+
+/**
+ * Indicates that a default name cannot be generated for
+ * a given type because no extension mapping has been configured
+ * via {@link Configuration#getExtensionMapping()}.  
+ * 
+ * @author <a href="mailto:andrew.rubinger@jboss.org">ALR</a>
+ * @version $Revision: $
+ */
+public class UnknownExtensionTypeException extends RuntimeException
+{
+
+   //-------------------------------------------------------------------------------------||
+   // Class Members ----------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   /**
+    * serialVersionUID
+    */
+   private static final long serialVersionUID = 1L;
+
+   //-------------------------------------------------------------------------------------||
+   // Constructor ------------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   /**
+    * Creates a new instance with message indicating the missing type
+    */
+   private <T extends Assignable> UnknownExtensionTypeException(final Class<T> type)
+   {
+      super(
+            "The current configuration has no mapping for type "
+                  + type.getCanonicalName()
+                  + ", unable to determine extension. Either add a mapping via Configuration.getExtensionMapping or manually assign a name.");
+   }
+
+   //-------------------------------------------------------------------------------------||
+   // Factory ----------------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
+   /**
+    * Creates a new {@link UnknownExtensionTypeException} for the specified type
+    * 
+    * @throws IllegalArgumentException If the type is not specified
+    */
+   static <T extends Assignable> UnknownExtensionTypeException newInstance(final Class<T> type)
+         throws IllegalArgumentException
+   {
+      // Precondition checks
+      if (type == null)
+      {
+         throw new IllegalArgumentException("type must be specified");
+      }
+
+      // Create new
+      return new UnknownExtensionTypeException(type);
+   }
+}
