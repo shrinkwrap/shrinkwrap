d47d7c164e4abe417be4d405d17107081620a620
Dan Allen <dan.j.allen@gmail.com>
2010-07-04 06:10:05 +0000

    SHRINKWRAP-201

---
+++ extension-jetty-7/src/test/java/org/jboss/shrinkwrap/jetty_7/servlet/RequestParamEchoServlet.java
@@ -0,0 +1,20 @@
+package org.jboss.shrinkwrap.jetty_7.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Dan Allen
+ */
+public class RequestParamEchoServlet extends HttpServlet
+{
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+   {
+      resp.getWriter().append(req.getParameter("echo"));
+   }
+
+}
