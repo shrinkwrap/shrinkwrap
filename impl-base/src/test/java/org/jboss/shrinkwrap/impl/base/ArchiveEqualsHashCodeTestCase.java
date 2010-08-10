4a414a3dde0150f1844a245727b9e6cc9c231e98
Chris Wash <chris.wash@gmail.com>
2010-08-10 17:03:51 -0400

    Tweak assertion messages.

--- impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/ArchiveEqualsHashCodeTestCase.java
+++ impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/ArchiveEqualsHashCodeTestCase.java
@@ -14,8 +14,7 @@ import org.jboss.shrinkwrap.impl.base.importer.ZipContentAssertionDelegate;
 import org.junit.Test;
 
 /**
- * Tests that verify {@link Archive} equals and hashCode implement the 
- * semantics of these methods correctly.
+ * Tests that verify {@link Archive} implement the semantics of equals and hashCode correctly.
  * 
  * SHRINKWRAP-181
  * 
@@ -26,7 +25,6 @@ import org.junit.Test;
 public class ArchiveEqualsHashCodeTestCase
 {
 
- 
    //-------------------------------------------------------------------------------------||
    // Class Members ----------------------------------------------------------------------||
    //-------------------------------------------------------------------------------------||
@@ -36,102 +34,104 @@ public class ArchiveEqualsHashCodeTestCase
     */
    private static final ZipContentAssertionDelegate delegate = new ZipContentAssertionDelegate();
 
-   
+   //-------------------------------------------------------------------------------------||
+   // Tests ------------------------------------------------------------------------------||
+   //-------------------------------------------------------------------------------------||
+
    @Test
    public void archiveEqualsShouldReturnTrueWhenNameAndContentsAreEqual() throws Exception
    {
-   
-      // Get the test file
+
       final File testFile = delegate.getExistingResource();
       final File testFile2 = delegate.getExistingResource();
-      
-      // Make a new archive via the default domain
+
       final Archive<?> archive = ShrinkWrap.createFromZipFile(JavaArchive.class, testFile);
       final Archive<?> archive2 = ShrinkWrap.createFromZipFile(JavaArchive.class, testFile2);
-      
-      // Should test that name and contents are equal
+
       Assert.assertEquals("Archives were not equal, but should be.", archive, archive2);
-      
+
    }
-   
+
    @Test
    public void archiveEqualsShouldReturnFalseWhenArchivesContentsAreNotEqual() throws Exception
    {
       final String archiveName = "test.zip";
       final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, archiveName);
-      
+
       final File testFile = delegate.getExistingResource();
       final JavaArchive archive2 = ShrinkWrap.createFromZipFile(JavaArchive.class, testFile);
-      
+
       Assert.assertFalse("Archives were equal, but should not have been - contents differ.", archive.equals(archive2));
-      
+
    }
-   
+
    @Test
    public void archiveEqualsShouldReturnFalseWhenArchivesNamesAreNotEqual() throws Exception
    {
-      
+
       final String archiveName = "test.war";
       final String archiveName2 = "test.jar";
-      
+
       final JavaArchive archive = ShrinkWrap.create(JavaArchive.class, archiveName);
       final JavaArchive archive2 = ShrinkWrap.create(JavaArchive.class, archiveName2);
-      
+
       Assert.assertFalse("Archives were equal, but should not have been - names differ.", archive.equals(archive2));
-      
+
    }
-   
+
    @Test
    public void enterpriseArchiveEqualsShouldReturnTrueWhenNameAndContentsAreEqual() throws Exception
    {
-      
+
       final File testFile1 = delegate.getExistingResource();
       final File testFile2 = delegate.getExistingResource();
 
       final EnterpriseArchive ear1 = ShrinkWrap.createFromZipFile(EnterpriseArchive.class, testFile1);
       final EnterpriseArchive ear2 = ShrinkWrap.createFromZipFile(EnterpriseArchive.class, testFile2);
-      
+
       Assert.assertEquals("EnterpriseArchive instances were not equal, but should be.", ear1, ear2);
-      
+
    }
-   
+
    @Test
    public void javaArchiveShouldEqualEnterpriseArchiveWhenNameAndContentsAreEqual() throws Exception
    {
       final File testFile1 = delegate.getExistingResource();
       final File testFile2 = delegate.getExistingResource();
-      
+
       final JavaArchive jar = ShrinkWrap.createFromZipFile(JavaArchive.class, testFile1);
       final EnterpriseArchive ear = ShrinkWrap.createFromZipFile(EnterpriseArchive.class, testFile2);
-      
-      Assert.assertEquals("Archives had same name and contents but were not equal", jar, ear);
-      
+
+      Assert.assertEquals("JavaArchive and EnterpriseArchive were not equal, but should be.", jar, ear);
+
    }
-   
+
    @Test
    public void archivesShouldHaveHashCodeConsistency() throws Exception
    {
       final File testFile1 = delegate.getExistingResource();
-      
+
       final JavaArchive jar = ShrinkWrap.createFromZipFile(JavaArchive.class, testFile1);
-      
-      Assert.assertEquals("Archive's hashCode did not return consistent value for same instance", jar.hashCode(), jar.hashCode());
-   }   
-   
+
+      Assert.assertEquals("Archive's hashCode did not return consistent value for same instance", jar.hashCode(),
+            jar.hashCode());
+   }
+
    @Test
    public void setShouldRestrictDuplicateArchives() throws Exception
    {
       final File testFile1 = delegate.getExistingResource();
-      
+
       final Archive<?> jar = ShrinkWrap.createFromZipFile(JavaArchive.class, testFile1);
       final Archive<?> jar2 = ShrinkWrap.createFromZipFile(JavaArchive.class, testFile1);
-      
+
       Set<Archive<?>> set = new HashSet<Archive<?>>();
       set.add(jar);
       set.add(jar2);
-      
-      Assert.assertEquals("Equals and hashCode semantics did not catch add of duplicate Archive entry to set.", 1, set.size());
-      
+
+      Assert.assertEquals("Equals and hashCode semantics did not catch add of duplicate Archive entry to set.", 1,
+            set.size());
+
    }
-   
+
 }
