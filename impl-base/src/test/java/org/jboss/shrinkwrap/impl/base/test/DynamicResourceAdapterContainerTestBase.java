0434358646a035f69a47bf371549fcd970ddec6e
Davide D'Alto <daltodavide@gmail.com>
2011-04-03 13:15:57 +0100

    [SHRINKWRAP-179] Add setResourceAdapterXML(Package, String)

--- impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/test/DynamicResourceAdapterContainerTestBase.java
+++ impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/test/DynamicResourceAdapterContainerTestBase.java
@@ -5,6 +5,7 @@ import junit.framework.Assert;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ArchivePath;
 import org.jboss.shrinkwrap.api.container.ResourceAdapterContainer;
+import org.jboss.shrinkwrap.impl.base.asset.AssetUtil;
 import org.jboss.shrinkwrap.impl.base.path.BasicPath;
 import org.junit.Test;
 
@@ -18,6 +19,17 @@ public abstract class DynamicResourceAdapterContainerTestBase<T extends Archive<
    @ArchiveType(ResourceAdapterContainer.class)
    public void testSetResourceAdapterXMLResource() throws Exception {
       getResourceAdapterContainer().setResourceAdapterXML(NAME_TEST_PROPERTIES);
+      
+      ArchivePath testPath = new BasicPath(getResourceAdapterPath(), "ra.xml");
+      Assert.assertTrue(
+            "Archive should contain " + testPath,
+            getArchive().contains(testPath));
+   }
+   
+   @Test
+   @ArchiveType(ResourceAdapterContainer.class)
+   public void testSetResourceAdapterXMLResourceInPackage() throws Exception {
+      getResourceAdapterContainer().setResourceAdapterXML(AssetUtil.class.getPackage(), "Test.properties");
       
       ArchivePath testPath = new BasicPath(getResourceAdapterPath(), "ra.xml");
       Assert.assertTrue(
