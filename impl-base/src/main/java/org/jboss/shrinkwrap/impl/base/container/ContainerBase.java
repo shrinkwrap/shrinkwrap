7f3a9d701e2ee0965eac55b14bdd959790bdfb06
Davide D'Alto <daltodavide@gmail.com>
2011-06-19 00:13:38 +0100

    [SHRINKWRAP-285] Adding nested JAR resources fails in Windows

--- impl-base/src/main/java/org/jboss/shrinkwrap/impl/base/container/ContainerBase.java
+++ impl-base/src/main/java/org/jboss/shrinkwrap/impl/base/container/ContainerBase.java
@@ -614,9 +614,7 @@ public abstract class ContainerBase<T extends Archive<T>> extends AssignableBase
 
       for (final ClassLoader classLoader : classLoaders)
       {
-         final String path = resource.getPath();
-         final String adjustedPath = path.substring(path.indexOf("!/") + 2, path.length());
-         final InputStream in = classLoader.getResourceAsStream(adjustedPath);
+         final InputStream in = classLoader.getResourceAsStream(resourceAdjustedPath(resource));
          if (in != null)
          {
             final Asset asset = new ByteArrayAsset(in);
@@ -624,6 +622,13 @@ public abstract class ContainerBase<T extends Archive<T>> extends AssignableBase
          }
       }
       throw new IllegalArgumentException(resource.getPath() + " was not found in any available ClassLoaders");
+   }
+
+   private String resourceAdjustedPath(final File resource)
+   {
+      final String path = resource.getPath();
+      final String adjustedPath = path.substring(path.indexOf("!" + File.separator) + 2, path.length());
+      return adjustedPath.replace(File.separator, "/");
    }
    
    /* (non-Javadoc)
