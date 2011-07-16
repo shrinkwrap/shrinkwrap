64a4503cc375b03b88038fa423ab03a9ba53baa0
Andrew Lee Rubinger <alr@jboss.org>
2011-07-16 00:45:39 -0400

    [SHRINKWRAP-305] Remove test which is no longer applicable due to changed internals

--- impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/ServiceExtensionLoaderTestCase.java
+++ impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/ServiceExtensionLoaderTestCase.java
@@ -16,24 +16,15 @@
  */
 package org.jboss.shrinkwrap.impl.base;
 
-import java.io.IOException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Enumeration;
 
 import junit.framework.Assert;
-import junit.framework.TestCase;
 
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.Assignable;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.UnknownExtensionTypeException;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -56,57 +47,6 @@ public class ServiceExtensionLoaderTestCase
       Assert.assertNotNull(extension);
 
       Assert.assertTrue(extension.getClass() == ExtensionImpl.class);
-   }
-
-   /**
-    * SHRINKWRAP-234
-    * 
-    * Ensures we don't use a cached ClassLoader in loading extension
-    * types; instead look to TCCL
-    */
-   @Test
-   @Ignore
-   // This might need to be either reworked or ignored; SHRINKWRAP-246 
-   // removes the internals which led to caching CLs in the first place.
-   // Examine further.
-   //TODO SHRINKWRAP-246 Before upstream push
-   public void useTcclInLoading()
-   {
-
-      // Initialize a loader
-      final ServiceExtensionLoader loader = createLoaderUsingTccl();
-      final JavaArchive archive = ShrinkWrap.create(JavaArchive.class);
-
-      // First ensure we can't load the Mock Archive.  Only our special ClassLoader 
-      // will know how.
-      boolean gotExpectedError = false;
-      try
-      {
-         loader.load(MockArchive.class, archive);
-      }
-      catch (final UnknownExtensionTypeException uete)
-      {
-         gotExpectedError = true;
-      }
-      TestCase.assertTrue("Test setup is broken, should not be able to load mock archive", gotExpectedError);
-
-      // Make a mock ClassLoader which knows how to handle our stuff
-      final ClassLoader cl = new MockArchiveDescriptorNameAdjustingClassLoader();
-      final ClassLoader originalCl = TestSecurityActions.getThreadContextClassLoader();
-      // Set the CL as TCCL
-      setTccl(cl);
-      try
-      {
-         // Try to load, using our special CL as TCCL, proving that the CL used by the loader is
-         // no longer cached
-         final Archive<?> loaded = loader.load(MockArchive.class, archive);
-         TestCase.assertNotNull("Archive should have been loaded", loaded);
-      }
-      finally
-      {
-         setTccl(originalCl);
-      }
-
    }
 
    @Test
@@ -173,72 +113,6 @@ public class ServiceExtensionLoaderTestCase
    public static interface MissingExtension extends Assignable
    {
 
-   }
-   
-   /**
-    * A {@link ClassLoader} which maps requests for {@link MockArchive} to its hidden, 
-    * adjusted name such that only this CL may be used in loading it via the 
-    * {@link ServiceExtensionLoader}. 
-    *
-    */
-   private static class MockArchiveDescriptorNameAdjustingClassLoader extends URLClassLoader
-   {
-
-      /**
-       * Name of the archive descriptor name we'll adjust
-       */
-      private static final String MOCK_ARCHIVE_DESCRIPTOR_NAME = "META-INF/services/" + MockArchive.class.getName();
-
-      /**
-       * Suffix we append to the mock descriptor so that only this CL will see it
-       */
-      private static final String MOCK_ARCHIVE_DESCRIPTOR_SUFFIX = "-MockClassLoader";
-
-      public MockArchiveDescriptorNameAdjustingClassLoader()
-      {
-         super(new URL[]
-         {}, TestSecurityActions.getThreadContextClassLoader());
-      }
-
-      /**
-       * Returns a descriptor if the requested resource is for a 
-       * META-INF/services/{@link MockArchive}
-       * @see java.net.URLClassLoader#findResource(java.lang.String)
-       */
-      @Override
-      public Enumeration<URL> getResources(String name) throws IOException
-      {
-         // Determine if we're asking for the mock archive descriptor
-         if (name.equals(MOCK_ARCHIVE_DESCRIPTOR_NAME))
-         {
-            // Add in the suffix and look for that
-            name = name + MOCK_ARCHIVE_DESCRIPTOR_SUFFIX;
-         }
-
-         // Use the super implementation
-         return super.getResources(name);
-      }
-   }
-   
-   /**
-    * Sets the TCCL to that supplied
-    * @param cl
-    */
-   private static void setTccl(final ClassLoader cl)
-   {
-      assert cl != null : "CL must be supplied.";
-      AccessController.doPrivileged(new PrivilegedAction<Void>()
-      {
-
-         @Override
-         public Void run()
-         {
-            Thread.currentThread().setContextClassLoader(cl);
-
-            // Return
-            return null;
-         }
-      });
    }
    
    /**
