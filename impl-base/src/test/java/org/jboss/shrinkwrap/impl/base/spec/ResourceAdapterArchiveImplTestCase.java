1205ff989d8bf3d2465464a6d34835c312475f5b
Andrew Lee Rubinger <alr@jboss.org>
2011-08-04 22:04:37 -0400

    [SHRINKWRAP-303] Default archive format of RAR is ZIP

--- impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/spec/ResourceAdapterArchiveImplTestCase.java
+++ impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/spec/ResourceAdapterArchiveImplTestCase.java
@@ -176,6 +176,6 @@ public class ResourceAdapterArchiveImplTestCase extends DynamicResourceAdapterCo
    @Override
    protected ArchiveFormat getExpectedArchiveFormat()
    {
-      return ArchiveFormat.UNKNOWN;
+      return ArchiveFormat.ZIP;
    }
 }
