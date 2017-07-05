85edcee4388bddc5aaa0fe38d2c7abecd9838c09
Falko Modler <Falko.Modler@t-systems.com>
2017-07-05 17:27:37 +0200

    SHRINKWRAP-515 Fix name and JDK5 compliance of AddPackage*Test
    - suffix must be TestCase to be picked up by surefire
    - removed JDK7 stuff
    - consolidated by introducing test base class
    - worked around file leak (?) on windows (just warn for now)

---
+++ impl-base/src/test/java/org/jboss/shrinkwrap/impl/base/spec/AddPackageFromWarTestCase.java
@@ -0,0 +1,33 @@
+package org.jboss.shrinkwrap.impl.base.spec;
+
+import junit.framework.Assert;
+
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.impl.base.spec.donotchange.DummyClassA;
+import org.junit.Test;
+
+/**
+ * @author Bob McWhirter
+ */
+public class AddPackageFromWarTestCase extends AddPackageTestBase {
+
+    @Override
+    protected Archive<?> buildArchive() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class, "my.war");
+        war.addClass(DummyClassA.class);
+        return war;
+    }
+
+    @Test
+    public void testAddPackage() {
+        // This archive's classloaders should be the app classloader (without `donotchange`)
+        // and an .war-based classloader including the `donotchange` within WEB-INF/classes/...
+
+        JavaArchive jar = this.domain.getArchiveFactory().create(JavaArchive.class);
+        jar.addPackage("org.jboss.shrinkwrap.impl.base.spec.donotchange" );
+        Assert.assertNotNull( jar.get( "/" + DummyClassA.class.getName().replace( '.', '/' ) + ".class" ) );
+    }
+}
